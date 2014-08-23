da41f7dab7b526f6196a85d9151ed42b29dee383
Denis S <rusdw@yandex.ru>
2014-08-24 01:25:50 +0300

    main script

---
+++ run_analysis.R
@@ -0,0 +1,54 @@
+# ------------------------------------------------------
+# R script for Getting and Cleaning Data Course Project
+
+# Note: run from directory containing 'test' and 'train'
+# subdirs with test and train sets 
+# ------------------------------------------------------
+
+# read data from train set
+x_train <- read.table("train/X_train.txt")
+y_train <- read.table("train/y_train.txt",col.names="ActivityID")
+subj_train <- read.table("train/subject_train.txt",col.names="SubjectID")
+
+# read data from test set
+x_test <- read.table("test/X_test.txt")
+y_test <- read.table("test/y_test.txt",col.names="ActivityID")
+subj_test <- read.table("test/subject_test.txt",col.names="SubjectID")
+
+# read list of feature names (561 features)
+flabels <- read.table("features.txt",,col.names=c("fid","fname"))
+
+# assign descriptive feature names to columns in 'x_train' and 'x_test' data frames 
+colnames(x_train) <- flabels$fname
+colnames(x_test) <- flabels$fname
+
+# add 'SubjectID' and 'ActivityID' columns to train set
+train <- cbind(x_train,subj_train,y_train)
+
+# add 'SubjectID' and 'ActivityID' columns to test set
+test <- cbind(x_test,subj_test,y_test)
+
+# merge train and test sets to one set
+wearable <- rbind(train,test)
+
+# identify features with mean- and standard deviation-based values
+featurestoextract<-c(grep("mean()",flabels$fname,fixed=T),grep("std()",flabels$fname,fixed=T))
+
+# get rid of all features expect those in 'featurestoextract' and 'SubjectID' and 
+# 'ActivityID' (these two are last columns) 
+wearable2 <- wearable[,c(featurestoextract,562,563)]
+
+# read descriptive activity names
+alabels <- read.table("activity_labels.txt",col.names=c("ActivityID","ActivityName"))
+
+# change activity-ids to activity names in 'wearable2' data frame
+wearable2$Activity <- sapply(wearable2$ActivityID, function(id) { as.factor(alabels$ActivityName[id]) })
+
+# remove 'ActivityID' column
+wearable2$ActivityID <- NULL
+
+# create final tidy dataset with average aggregation on 'ActivityID' and 'SubjectID' columns
+final_tidy_data_set <- aggregate(. ~ Activity + SubjectID, wearable2, FUN=mean) 
+
+# write final tidy dataset to disk
+write.table(final_tidy_data_set, file = "final-tidy-data-set.txt", row.names = F)
